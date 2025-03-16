22ee2e518dce6461e2f3dc58c1fb8d946aae8b76
Thanh Mai <thanhmai.duongphan1998@gmail.com>
2025-03-16 21:09:54 +0700

    up code tuan 3

--- src/test/java/Browser/EdgeTest.java
+++ src/test/java/Browser/EdgeTest.java
@@ -2,12 +2,44 @@ package Browser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EdgeTest {
     @Test
     void openWithDefaultMode() {
         WebDriver driver = new EdgeDriver();
-        driver.get("https://www.google.com/");
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+    }
+
+    @Test
+    void openBrowserWithHeadlessMode() {
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--headless=new");
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        System.out.println("Page Title: " + driver.getTitle());
+    }
+
+    @Test
+    void openBrowserWithMobileViewMode(){
+        Map<String, Object> deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width", 344);
+        deviceMetrics.put("height", 882);
+        Map<String, Object> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceMetrics", deviceMetrics);
+
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
     }
 }
